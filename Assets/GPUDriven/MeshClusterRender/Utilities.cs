e38b4c2d010bed59923775a42055ee1364ce6160
DESKTOP-I51QT31\IGG <935437130@qq.com>
2023-03-16 16:15:42 +0800

    begin normal cone cull

---
+++ Assets/GPUDriven/MeshClusterRender/Utilities.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Utilities : MonoBehaviour
+{
+    /// <summary>
+    /// 获取最小的包围球
+    /// 参考：https://github.com/microsoft/DirectX-Graphics-Samples/blob/e5ea2ac7430ce39e6f6d619fd85ae32581931589/Samples/Desktop/D3D12MeshShaders/src/MeshletGenerator/Utilities.cpp#L304
+    /// </summary>
+    /// <param name="normals"></param>
+    Vector4 MinimumBoundingSphere(Vector3[] points)
+    {
+        
+        return Vector4.one;
+    }
+}
